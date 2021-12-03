45259602c1fb3b6b52c12b9288aefd3dd483e86f
zhiqianzhou <540894485@qq.com>
2021-12-03 11:17:50 +0800

    [wip] remove torch geometric. the first step

--- crowd_nav/policy/graph_model.py
+++ crowd_nav/policy/graph_model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.nn.functional import softmax, relu
 from torch.nn import Parameter
 from crowd_nav.policy.helpers import mlp, GAT #, GraphAttentionLayer
-from torch_geometric.nn import GATConv
+# from torch_geometric.nn import GATConv
 
 
 class RGL(nn.Module):
@@ -259,36 +259,36 @@ class GraphAttentionLayer(nn.Module):
         A = self.w_a(pairwise_features).reshape(-1, X.size(1), X.size(1))
         return A
 
-class PGAT(torch.nn.Module):
-    def __init__(self, num_features, n_classes, num_heads, dropout, num_hidden, num_hidden_layers, activation, concat=True, neg_slope=0.2, bias=True):
-        super(PGAT, self).__init__()
-        # Activation
-        self.activation = activation
-        # input layer
-        self.gat_input = GATConv(num_features, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
-        # Hidden layers
-        self.layers = nn.ModuleList()
-        for _ in range(num_hidden_layers):
-            if concat:
-                self.layers.append(GATConv(num_hidden*num_heads, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias))
-            else:
-                self.layers.append(GATConv(num_hidden, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias))
-        # output layer
-        if concat:
-            self.gat_output = GATConv(num_hidden*num_heads, n_classes, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
-        else:
-            self.gat_output = GATConv(num_hidden, n_classes, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
-
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        x = self.gat_input(x, edge_index)
-        x = self.activation(x)
-        for layer in  self.layers:
-            x = layer(x, edge_index)
-            x = self.activation(x)
-        x = self.gat_output(x, edge_index)
-        x = x.mean(1).unsqueeze(dim=1)
-        return torch.tanh(x)
+# class PGAT(torch.nn.Module):
+#     def __init__(self, num_features, n_classes, num_heads, dropout, num_hidden, num_hidden_layers, activation, concat=True, neg_slope=0.2, bias=True):
+#         super(PGAT, self).__init__()
+#         # Activation
+#         self.activation = activation
+#         # input layer
+#         self.gat_input = GATConv(num_features, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
+#         # Hidden layers
+#         self.layers = nn.ModuleList()
+#         for _ in range(num_hidden_layers):
+#             if concat:
+#                 self.layers.append(GATConv(num_hidden*num_heads, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias))
+#             else:
+#                 self.layers.append(GATConv(num_hidden, num_hidden, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias))
+#         # output layer
+#         if concat:
+#             self.gat_output = GATConv(num_hidden*num_heads, n_classes, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
+#         else:
+#             self.gat_output = GATConv(num_hidden, n_classes, heads=num_heads, concat= concat, negative_slope=neg_slope, dropout = dropout, bias=bias)
+#
+#     def forward(self, data):
+#         x, edge_index = data.x, data.edge_index
+#         x = self.gat_input(x, edge_index)
+#         x = self.activation(x)
+#         for layer in  self.layers:
+#             x = layer(x, edge_index)
+#             x = self.activation(x)
+#         x = self.gat_output(x, edge_index)
+#         x = x.mean(1).unsqueeze(dim=1)
+#         return torch.tanh(x)
 
 class PG_GAT_RL(nn.Module):
     def __init__(self, config, robot_state_dim, human_state_dim):
