843d3db2bf7e2758ddb8eea63e9b9bc598012e8c
zhouzhiqian <540894485@qq.com>
2021-12-06 23:36:36 +0800

    [wip] multihead version. now the number of head is set to 2

--- crowd_nav/policy/graph_model.py
+++ crowd_nav/policy/graph_model.py
@@ -313,12 +313,13 @@ class PG_GAT_RL(nn.Module):
         self.state_dim = self.robot_state_dim + self.human_state_dim
         self.num_layer = num_layer
         self.X_dim = X_dim
+        self.hidden_dim = 16
         self.layerwise_graph = layerwise_graph
         self.skip_connection = skip_connection
-        self.gatinput = GraphAttentionLayer2(self.state_dim, self.X_dim)
-        self.gat0 = GraphAttentionLayer2(self.X_dim, self.X_dim)
-        self.gat1 = GraphAttentionLayer2(self.X_dim, self.X_dim)
-        self.gat2 = GraphAttentionLayer2(self.X_dim, self.X_dim)
+        self.gatinput = GATMultihead(self.state_dim, self.hidden_dim, self.X_dim, 2)
+        self.gat0 = GATMultihead(self.X_dim, self.hidden_dim, self.X_dim, 2)
+        # self.gat1 = GATMultihead(self.X_dim, self.hidden_dim, self.X_dim, 4)
+        # self.gat2 = GraphAttentionLayer2(self.X_dim, self.X_dim)
         logging.info('Similarity_func: {}'.format(self.similarity_function))
         logging.info('Layerwise_graph: {}'.format(self.layerwise_graph))
         logging.info('Skip_connection: {}'.format(self.skip_connection))
@@ -361,20 +362,36 @@ class PG_GAT_RL(nn.Module):
         else:
             adj = self.compute_adjectory_matrix(state)
             # compute feature matrix X
-
             if state.shape[0]==1:
-                H1, self.attention_weights = self.gatinput(state, adj)
+                H1 = self.gatinput(state, adj)
             else:
-                H1, _ = self.gatinput(state, adj)
-            H2, _ = self.gat0(H1, adj)
-            H3, _ = self.gat1(H2, adj)
-            H4, _ = self.gat2(H3, adj)
+                H1 = self.gatinput(state, adj)
+            H2 = self.gat0(H1, adj)
+            # H3 = self.gat1(H2, adj)
+            # H4, _ = self.gat2(H3, adj)
             if self.skip_connection:
-                output = H1 + H2 + H3 + H4
+                output = H1 + H2
             else:
                 output = H2
             output = torch.cat((state, output), dim=2)
             return output
+
+class GATMultihead(nn.Module):
+    def __init__(self, nfeat, nhid, noutput, nheads):
+        """Dense version of GAT."""
+        super(GATMultihead, self).__init__()
+
+        self.attentions = [GraphAttentionLayer2(nfeat, nhid, concat=True) for _ in range(nheads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+
+        self.out_att = GraphAttentionLayer2(nhid * nheads, noutput, concat=False)
+
+    def forward(self, x, adj):
+        # x = nn.functional.dropout(x, self.dropout, training=self.training)
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=2)
+        x = relu(self.out_att(x, adj))
+        return x
 
 
 class GraphAttentionLayer2(nn.Module):
@@ -391,11 +408,11 @@ class GraphAttentionLayer2(nn.Module):
         self.concat = concat
 
         self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
-        nn.init.orthogonal(self.W.data, gain=1.414)
+        nn.init.orthogonal_(self.W.data, gain=1.414)
         self.a = Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.orthogonal(self.a.data, gain=1.414)
+        nn.init.orthogonal_(self.a.data, gain=1.414)
         self.bias = nn.Parameter(torch.zeros(size=(1, out_features)))
-        nn.init.orthogonal(self.bias.data, gain=1.414)
+        nn.init.orthogonal_(self.bias.data, gain=1.414)
         self.leakyrelu = nn.LeakyReLU(negative_slope=0.1)
 
     def forward(self, input, adj):
@@ -416,4 +433,52 @@ class GraphAttentionLayer2(nn.Module):
         attention = nn.functional.softmax(attention, dim=2)
         h_prime = torch.matmul(attention, h)
         h_prime = h_prime + self.bias
-        return nn.functional.elu(h_prime), attention[0, 0, :].data.cpu().numpy()
+        return nn.functional.elu(h_prime)
+
+
+# class GraphAttentionLayer3(nn.Module):
+#     """
+#     Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+#     """
+#     def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+#         super(GraphAttentionLayer3, self).__init__()
+#         self.dropout = dropout
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.alpha = alpha
+#         self.concat = concat
+#
+#         self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+#         nn.init.xavier_uniform_(self.W.data, gain=1.414)
+#         self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))
+#         nn.init.xavier_uniform_(self.a.data, gain=1.414)
+#
+#         self.leakyrelu = nn.LeakyReLU(self.alpha)
+#
+#     def forward(self, h, adj):
+#         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
+#         e = self._prepare_attentional_mechanism_input(Wh)
+#
+#         zero_vec = -9e15*torch.ones_like(e)
+#         attention = torch.where(adj > 0, e, zero_vec)
+#         attention = nn.functional.softmax(attention, dim=1)
+#         h_prime = torch.matmul(attention, Wh)
+#
+#         if self.concat:
+#             return F.elu(h_prime)
+#         else:
+#             return h_prime
+#
+#     def _prepare_attentional_mechanism_input(self, Wh):
+#         # Wh.shape (N, out_feature)
+#         # self.a.shape (2 * out_feature, 1)
+#         # Wh1&2.shape (N, 1)
+#         # e.shape (N, N)
+#         Wh1 = torch.matmul(Wh, self.a[:self.out_features, :])
+#         Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
+#         # broadcast add
+#         e = Wh1 + Wh2.T
+#         return self.leakyrelu(e)
+#
+#     def __repr__(self):
+#         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
