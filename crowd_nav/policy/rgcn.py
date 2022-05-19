fc73882e80f980d60b321ba8b43339809ffd97d3
zhouzhiqian <540894485@qq.com>
2022-05-19 16:44:34 +0800

    [wip] build up our rgat

--- crowd_nav/policy/rgcn.py
+++ crowd_nav/policy/rgcn.py
@@ -9,6 +9,33 @@ from dgl.nn.pytorch.conv.graphconv import GraphConv
 from dgl.nn.pytorch.conv.gatconv import GATConv
 from crowd_nav.policy.helpers import mlp
 import dgl
+from crowd_nav.policy.gnn_models import HoRelGAT
+class RGATLayer(nn.Module):
+
+    def __init__(self,
+                 in_feat,
+                 out_feat,
+                 num_heads,
+                 rel_names,
+                 activation=None,
+                 dropout=0.0,
+                 bias=True,):
+        super(RGATLayer, self).__init__()
+        self.in_feat = in_feat
+        self.out_feat = out_feat
+        self.num_heads= num_heads
+        self.conv = dgl.nn.HeteroGraphConv({
+            rel: dgl.nn.pytorch.GATConv(in_feat, out_feat, num_heads=num_heads, bias=bias, allow_zero_in_degree=True)
+            for rel in rel_names
+        })
+
+    def forward(self, g, h_dict):
+        h_dict = self.conv(g, h_dict)
+        out_put = {}
+        for n_type, h in h_dict.items():
+            out_put[n_type] = h.squeeze()
+        return out_put
+
 class RGCN(nn.Module):
     def __init__(self, g, gnn_layers, in_dim, out_dim, hidden_dimensions, num_rels, activation,  final_activation,
                  feat_drop, num_bases=-1):
@@ -25,9 +52,9 @@ class RGCN(nn.Module):
         self.final_activation = torch.nn.ReLU()
         self.gnn_layers = gnn_layers
         self.use_rgcn = False
-        self.use_gat = True
+        self.use_gat = False
         self.use_gcn = False
-        self.use_rgat = False
+        self.use_rgat = True
         # create RGCN layers
         self.build_model()
 
@@ -69,15 +96,19 @@ class RGCN(nn.Module):
 
     def build_i2o_layer(self):
         if self.use_rgcn is True:
-            print('Building an I2O  layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
+            print('Building an RGCN I2O layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
             return RelGraphConv(self.encoder_dim[-1], self.out_dim, self.num_rels,
                                 dropout=self.feat_drop, num_bases=self.num_bases, activation=self.final_activation)
         elif self.use_gcn is True:
-            print('Building an I2O  layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
+            print('Building an GCN I2O layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
             return GraphConv(self.encoder_dim[-1], self.out_dim, activation=self.final_activation)
         elif self.use_gat is True:
-            print('Building an I2O  layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
+            print('Building an  GAT I2O  layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
             return GATConv(self.encoder_dim[-1], self.out_dim, num_heads=1, activation=self.final_activation)
+        elif self.use_rgat is True:
+            print('Building an RGAT I2O  layer of {}x{}'.format(self.encoder_dim[-1], self.out_dim))
+            return HoRelGAT(self.encoder_dim[-1], self.out_dim, self.num_rels,
+                                dropout=self.feat_drop, num_bases=self.num_bases, activation=self.final_activation)
 
 
 
@@ -94,6 +125,9 @@ class RGCN(nn.Module):
                 state_graph = dgl.add_self_loop(state_graph)
                 h1 = layer(state_graph, output)
                 h1 = h1.reshape(-1, self.out_dim)
+                output = output + h1
+            elif self.use_rgat:
+                h1 = layer(state_graph, output, edgetypes)
                 output = output + h1
         ## skip connection???
         return output
