85b2ab6a88c9a18bb4eac022ff66fd2aca3bb02f
zhouzhiqian <540894485@qq.com>
2022-06-06 07:55:14 +0800

    [wip] three phases.

--- crowd_sim/envs/crowd_sim.py
+++ crowd_sim/envs/crowd_sim.py
@@ -145,7 +145,7 @@ class CrowdSim(gym.Env):
         if self.phase_num == 0:
             self.static_obstacle_num = 3
             self.wall_num = 2
-            self.human_num = 7
+            self.human_num = 1
         elif self.phase_num == 1:
             self.static_obstacle_num = 3
             self.wall_num = 2
@@ -157,7 +157,7 @@ class CrowdSim(gym.Env):
         elif self.phase_num == 3:
             self.static_obstacle_num = 3
             self.wall_num = 2
-            self.human_num = 7
+            self.human_num = 5
         elif self.phase_num == 10: #for test
             self.static_obstacle_num = 3
             self.wall_num = 0
