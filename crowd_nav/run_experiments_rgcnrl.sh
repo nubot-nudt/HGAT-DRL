4be0d22ed2c38ee2db9374792a28cd0258d4ccb3
lixiao <lx852357@outlook.com>
2022-06-08 08:01:43 +0800

    compare

--- crowd_nav/run_experiments_rgcnrl.sh
+++ crowd_nav/run_experiments_rgcnrl.sh
@@ -7,7 +7,7 @@ c=0.25
 d=1.0
 e=0.02
 # Script to reproduce results
-for ((i=0;i<5;i+=1))
+for ((i=0;i<1;i+=1))
 do
 	python train.py \
 	--policy rgcn_rl \
@@ -21,6 +21,46 @@ do
 	--re_rvo  $e    \
 	--human_num 5  \
         --gnn gat	
+
+	python train.py \
+	--policy rgcn_rl \
+	--output_dir data/$day/gat/$i \
+	--randomseed $i  \
+	--config configs/icra_benchmark/rgcnrl.py \
+	--safe_weight $d \
+	--goal_weight $a \
+	--re_collision $b \
+	--re_arrival $c \
+	--re_rvo  $e    \
+	--human_num 5  \
+        --gnn rgcn	
+
+	python train.py \
+	--policy rgcn_rl \
+	--output_dir data/$day/gcn/$i \
+	--randomseed $i  \
+	--config configs/icra_benchmark/rgcnrl.py \
+	--safe_weight $d \
+	--goal_weight $a \
+	--re_collision $b \
+	--re_arrival $c \
+	--re_rvo  $e    \
+	--human_num 5  \
+        --gnn gcn	
+
+	python train.py \
+	--policy rgcn_rl \
+	--output_dir data/$day/transformer/$i \
+	--randomseed $i  \
+	--config configs/icra_benchmark/rgcnrl.py \
+	--safe_weight $d \
+	--goal_weight $a \
+	--re_collision $b \
+	--re_arrival $c \
+	--re_rvo  $e    \
+	--human_num 5  \
+        --gnn transformer
+
 
 #	python train.py \
 #	--policy model-predictive-rl \
