019fb5063f1049d361ca2439dab318573cf08506
zhouzhiqian <540894485@qq.com>
2022-06-09 09:20:06 +0800

    [wip] temp version in nubot1

--- crowd_sim/envs/crowd_sim.py
+++ crowd_sim/envs/crowd_sim.py
@@ -535,7 +535,7 @@ class CrowdSim(gym.Env):
             robot_theta = (np.random.random() - 0.5) * 2 * np.pi
         elif self.phase_num <= 0:
             target_x = (np.random.random() - 0.5) * self.square_width * 0.8
-            target_y = self.circle_radius
+            target_y = self.circle_radius - np.random.random() * 6
             robot_theta = np.pi / 2 + np.random.random() * np.pi / 4.0 - np.pi / 8.0
             # target_x = 0
             # target_y = self.circle_radius
